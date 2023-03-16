457cbde2d02bebf95fa86f7f7b7774945c254402
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-16 17:37:56 +0000

    Exe1

---
+++ ChangeString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a sentence.");
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine("Type a character to remove.");
+            string ChrRem = Console.ReadLine();
+
+            string character = sentence.Remove(ChrRem);
+            
+
+            Console.WriteLine(character);
+        }
+    }
+}
