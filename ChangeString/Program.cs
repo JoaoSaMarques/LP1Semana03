26d94dc3c5ad895a79b0744bf40db98ebf2d1402
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-16 18:09:23 +0000

    Exe1

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,12 +6,17 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
+            //Write sentence here
             String header = Console.ReadLine();
 
+            //Write letter here
             string remove = Console.ReadLine();
+            //Converts letter into char
             char character = char.Parse(remove);
 
+            //Write sentence
             Console.WriteLine(header);
+            //Write sentence with char removed
             Console.WriteLine(header.Trim( new Char[] { character } ));
         }
     }
