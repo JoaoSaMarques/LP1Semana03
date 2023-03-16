e3af3b431cae81b4d67e8b9dc2b921c8769e9910
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-16 18:42:39 +0000

    Exe3

---
+++ PlayerPerks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PlayerPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            [ Flags ]
+            enum Perks;
+            {
+                WaterBreathing = "w"
+                Stealth = "a"
+                AutoHeal = "s"
+                DoubleJump = "d"
+            }
+
+            string PlayerPerk = Console.ReadLine();
+
+            if PlayerPerk = "w"
+                
+
+
+
+            Console.WriteLine(PlayerPerk)
+
+
+
+        }
+
+
+    }
+}
