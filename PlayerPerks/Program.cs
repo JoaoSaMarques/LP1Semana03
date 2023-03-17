a8f7eab5ba6b196b430bd07443059f309b040647
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-17 10:10:02 +0000

    Exe3

--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -2,29 +2,63 @@
 
 namespace PlayerPerks
 {
+    [ Flags ]
+    enum Perks;
+    {
+        NoPerks = 0,
+        WaterBreathing = 1,
+        Stealth = 2,
+        AutoHeal = 4,
+        DoubleJump = 8,
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            [ Flags ]
-            enum Perks;
+            if (args.length == 0)
             {
-                WaterBreathing = "w"
-                Stealth = "a"
-                AutoHeal = "s"
-                DoubleJump = "d"
+                Console.WriteLine("You have no perks.");
+                return;
             }
 
-            string PlayerPerk = Console.ReadLine();
+            Perks Player = Perks.NoPerks;
 
-            if PlayerPerk = "w"
-                
+            foreach(char c in args[0])
+            {
+                switch (c)
+                {
+                    case "w":
+                        Player ^= Perks.WaterBreathing;
+                        break;
+                    case "a":
+                        Player ^= Perks.AutoHeal;
+                        break;
+                    case "s":
+                        Player ^= Perks.Stealth;
+                        break;
+                    case "d":
+                        Player ^= Perks.DoubleJump;
+                        break;
+                    default:
+                        Console.WriteLine("I do not recognize this perk")
+                        return;
+                }
+            }
 
+            Console.WriteLine("You have acquired the following perks: ")
 
+            if ((Player & Perks.Stealth) == Perks.Stealth && 
+            (Player & Perks.DoubleJump) == Perks.DoubleJump)
+            {
+                Console.WriteLine("Silent Jumper perk");
+            }
+            if ((Player & Perks.AutoHeal) != Perks.AutoHeal)
+            {
+                Console.WriteLine("Not gonna make it!");
+            }
 
-            Console.WriteLine(PlayerPerk)
-
-
+            Console.ReadLine();
 
         }
 
