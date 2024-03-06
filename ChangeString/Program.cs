912908390e8d4ecafc3271b7e85f26736a2c6229
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-06 10:50:47 +0000

    solicita caracter e string

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,10 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please input a string:");
+            string s = Console.ReadLine();
+            Console.WriteLine("Please input a char:");
+            string c = Console.ReadLine();
         }
     }
 }
