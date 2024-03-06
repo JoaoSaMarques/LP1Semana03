9c7cb2ae9944c734107831f59143cd1184548abd
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-06 11:26:18 +0000

    matriz a e vetor b + for loop

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -8,8 +8,20 @@ namespace ArrayMult
         {
             if (args.Length != 6)
             {
-                Console.WriteLine("Por favor, forneça exatamente 6 argumentos para os valores da matriz A e vetor b.");
+                Console.WriteLine("Input exactly 6 arguments");
                 return;
+            }
+
+            // Matriz A e Vetor B
+            float[,] A = new float[2, 2];
+            float[] b = new float[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    A[i, j] = float.Parse(args[i * 2 + j]);
+                }
             }
 
             
