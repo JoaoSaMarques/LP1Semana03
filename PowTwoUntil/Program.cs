4608c7fbc9684b3542d0f692f47272e22893b4bf
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-06 11:59:25 +0000

    Added powersof2untiiln and called it twice in main

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -8,9 +8,19 @@ namespace PowTwoUntil
         {
             PowersOf2Until5();
             PowersOf2Until5();
+            PowersOf2UntilN();
+            PowersOf2UntilN();
         }
 
         private static void PowersOf2Until5()
+        {
+            for(int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void PowersOf2UntilN()
         {
             for(int i = 1; i <= (1 << 5); i = i << 1)
             {
