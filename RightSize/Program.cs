83ea94e0bdfa21f0f2d336e2460d93297b7027a5
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-06 10:42:42 +0000

    if superior to 3 characters, write the arguments

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -6,7 +6,13 @@ namespace RightSize
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            foreach (string argument in args)
+            {
+                if (argument.Length > 3)
+                {
+                    Console.WriteLine(argument);
+                }
+            }
         }
     }
 }
