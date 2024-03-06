8f07db96685fa307df85d9a5e70e7f8eee438855
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-06 10:44:08 +0000

    if length higher than 6, break

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -8,9 +8,16 @@ namespace RightSize
         {
             foreach (string argument in args)
             {
+                // If character length is higher than 3, write chars
                 if (argument.Length > 3)
                 {
                     Console.WriteLine(argument);
+                }
+
+                // If character length is higher than 6 stop
+                if (argument.Length > 6)
+                {
+                    break;
                 }
             }
         }
