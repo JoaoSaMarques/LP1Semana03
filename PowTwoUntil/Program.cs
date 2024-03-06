285f36d5c231fa60740eebed6a0c5d73e865263b
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-06 12:11:29 +0000

    Added xml

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -4,19 +4,30 @@ namespace PowTwoUntil
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args">it will go through and call the methods</param>
         static void Main(string[] args)
         {
             PowersOf2Until5();
             PowersOf2Until5();
-            PowersOf2UntilN(6); 
-            PowersOf2UntilN(4); 
+            PowersOf2UntilN(5); 
+            PowersOf2UntilN(3); 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         private static void PowersOf2Until5()
         {
             PowersOf2UntilN(5);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="n">Write until it reaches number n</param>
         private static void PowersOf2UntilN(int n)
         {
             for(int i = 1; i <= (1 << n); i = i << 1)
