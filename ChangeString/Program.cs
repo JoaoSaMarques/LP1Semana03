ccae66c2e8206d6e2d53be7986b718e885dd228b
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-06 11:01:22 +0000

    char selected is replaced with letter x

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,10 +6,17 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
+            //Ask for string
             Console.WriteLine("Please input a string:");
             string s = Console.ReadLine();
+
+            //Ask for char
             Console.WriteLine("Please input a char:");
-            string c = Console.ReadLine();
+            char c = Console.ReadKey().KeyChar;
+
+            //Replace the letter c in string s with the letter x
+            string modifiedString = s.Replace(c, 'x');
+            Console.WriteLine(modifiedString);
         }
     }
 }
