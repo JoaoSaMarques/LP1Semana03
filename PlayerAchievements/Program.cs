09d17c01e9d055e5bd620d06f768683d60efaf67
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-08 16:06:31 +0000

    Added enumeration flag

--- PlayerAchievements/Program.cs
+++ PlayerAchievements/Program.cs
@@ -2,6 +2,14 @@
 
 namespace PlayerAchievements
 {
+    [Flags]
+    internal enum HeroPerks
+    {
+        DefeatOptionalBoss = 1
+        FindHiddenLevel = 2
+        FinishGame = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
