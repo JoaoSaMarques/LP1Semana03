131b7521ed330334e98b4e4cd3f50e97cda6513b
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-08 16:17:03 +0000

    create an array and ask one by one

--- PlayerAchievements/Program.cs
+++ PlayerAchievements/Program.cs
@@ -5,8 +5,8 @@ namespace PlayerAchievements
     [Flags]
     internal enum Achievements
     {
-        DefeatOptionalBoss = 1
-        FindHiddenLevel = 2
+        DefeatOptionalBoss = 1,
+        FindHiddenLevel = 2,
         FinishGame = 3
     }
 
@@ -14,10 +14,39 @@ namespace PlayerAchievements
     {
         static void Main(string[] args)
         {
-            Achievements achievements = 0
+            Console.WriteLine("Please input the number of players:");
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please input the number of players.")
-            int n = Console.ReadLine()
+            Achievements[] achievementArray = new Achievements[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Player {i + 1}: achievements (D, F, G):");
+                string input = Console.ReadLine().ToUpper();
+
+                Achievements playerAchievements = 0;
+
+                foreach (char achievementCode in input)
+                {
+                    switch (achievementCode)
+                    {
+                        case 'D':
+                            playerAchievements |= Achievements.DefeatOptionalBoss;
+                            break;
+                        case 'F':
+                            playerAchievements |= Achievements.FindHiddenLevel;
+                            break;
+                        case 'G':
+                            playerAchievements |= Achievements.FinishGame;
+                            break;
+                        default:
+                            Console.WriteLine($"Invalid achievement code: {achievementCode}");
+                            break;
+                    }
+                }
+
+                achievementArray[i] = playerAchievements;
+            }
         }
     }
 }
