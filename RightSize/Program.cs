df39274eb411be383b5e2c3462856649cc76e210
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-03-05 14:11:51 +0000

    add comments

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -10,12 +10,15 @@ namespace RightSize
             {
                 if (s.Length >= 8)
                 {
+                    // Write this if the requirements are not met
                     Console.WriteLine("[EARLY STOP]");
+                    // Exit the program
                     return;
                 }
 
                 else if (s.Length > 3)
                 {
+                    // Write like normal
                     Console.WriteLine(s);
                 }
             }
