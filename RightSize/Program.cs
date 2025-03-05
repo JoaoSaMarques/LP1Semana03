35c548925e6d4471ebaa414bf9917d9c7d40b575
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-03-05 14:07:07 +0000

    add args.length checker

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -6,7 +6,10 @@ namespace RightSize
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("", args[i]);
+            }
         }
     }
 }
