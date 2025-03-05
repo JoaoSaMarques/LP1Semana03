6d0992aa40aec9e66942f7825d675f79a4da4cc5
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-03-05 14:44:27 +0000

    for each playerPerk

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -15,6 +15,29 @@ namespace HeroPerk
             string input = args[0];
             Perks playerPerks = 0;
             bool hasInvalidChar = false;
+
+            foreach (char c in input)
+            {
+                switch (c)
+                {
+                    case 'w':
+                        playerPerks ^= Perks.WarpShift; // Toggle perk
+                        break;
+                    case 'a':
+                        playerPerks ^= Perks.Stealth; // Toggle perk
+                        break;
+                    case 's':
+                        playerPerks ^= Perks.AutoHeal; // Toggle perk
+                        break;
+                    case 'd':
+                        playerPerks ^= Perks.DoubleJump; // Toggle perk
+                        break;
+                    default:
+                        hasInvalidChar = true;
+                        break;
+                }
+            }
+
         }
     }
 }
